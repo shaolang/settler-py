4489cdee04a3df358e75be6f66f0634995063295
shaolang <shaolang@ymail.com>
2021-09-09 07:14:35 +0800

    Add dynamnic weekends property settler.spot

--- src/settler/__init__.py
+++ src/settler/__init__.py
@@ -3,10 +3,10 @@ from datetime import timedelta
 ONE_DAY = timedelta(days=1)
 TWO_DAYS = timedelta(days=2)
 
-def spot(trade_date):
+def spot(trade_date, weekends=[6, 7]):
     result = trade_date + TWO_DAYS
 
-    while result.isoweekday() > 5:
+    while result.isoweekday() in weekends:
         result += ONE_DAY
 
     return result
--- test/spot_test.py
+++ test/spot_test.py
@@ -7,3 +7,9 @@ import settler as s
 @given(st.dates(date(2000, 1, 1), date(2050, 12, 31)))
 def test_spot_date_never_falls_on_weekend(trade_date):
     assert s.spot(trade_date).isoweekday() < 6
+
+
+@given(st.lists(st.integers(), max_size=3, unique=True),
+        st.dates(date(2000, 1, 1), date(2050, 12, 31)))
+def test_weekends_arent_always_sat_and_sun_only(weekends, trade_date):
+    assert s.spot(trade_date, weekends).isoweekday() not in weekends
