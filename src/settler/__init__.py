eb4f9d9ee171e5e4b0432d2506973876246b8a9d
shaolang <shaolang@ymail.com>
2021-09-09 07:36:45 +0800

    Ensure spot don't fall on USD holidays

--- src/settler/__init__.py
+++ src/settler/__init__.py
@@ -3,10 +3,10 @@ from datetime import timedelta
 ONE_DAY = timedelta(days=1)
 TWO_DAYS = timedelta(days=2)
 
-def spot(trade_date, weekends=[6, 7]):
+def spot(trade_date, weekends=[6, 7], *, usd_holidays=[]):
     result = trade_date + TWO_DAYS
 
-    while result.isoweekday() in weekends:
+    while result.isoweekday() in weekends or result in usd_holidays:
         result += ONE_DAY
 
     return result
--- test/spot_test.py
+++ test/spot_test.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, timedelta
 from hypothesis import given
 
 import hypothesis.strategies as st
@@ -8,6 +8,14 @@ import settler as s
 
 def trade_dates():
     return st.dates(date(2000, 1, 1), date(2050, 12, 31))
+
+
+@st.composite
+def trade_dates_and_usd_holidays(draw):
+    trade_date = draw(trade_dates())
+    days_to_add = draw(st.lists(st.integers(0, 20).map(lambda n: trade_date + timedelta(days=n))))
+
+    return (trade_date, days_to_add)
 
 def weekend_lists():
     return st.lists(st.integers(1, 7), max_size=3, unique=True)
@@ -22,3 +30,9 @@ def test_spot_date_never_falls_on_weekend(trade_date):
 @given(weekend_lists(), trade_dates())
 def test_weekends_arent_always_sat_and_sun_only(weekends, trade_date):
     assert s.spot(trade_date, weekends).isoweekday() not in weekends
+
+
+@given(trade_dates_and_usd_holidays())
+def test_spot_never_falls_on_usd_holidays(trade_date_and_usd_holidays):
+    trade_date, usd_holidays = trade_date_and_usd_holidays
+    assert s.spot(trade_date, usd_holidays=usd_holidays) not in usd_holidays
