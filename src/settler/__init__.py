2f634e0f9e88ebe82fe511f64711c5fb54778ba8
shaolang <shaolang@ymail.com>
2021-09-09 09:39:00 +0800

    Replace hard-code default with constant

--- src/settler/__init__.py
+++ src/settler/__init__.py
@@ -3,19 +3,20 @@ from datetime import timedelta
 
 class ValueDateCalculator:
     __DEFAULT_WEEKENDS = [6, 7]
+    __DEFAULT_HOLIDAYS = []
     __ONE_DAY = timedelta(days=1)
+    __DEFAULT_SPOT_LAG = timedelta(days=2)
     __TWO_DAYS = timedelta(days=2)
 
     def __init__(self):
-        self.__spot_lag = timedelta(days=2)
         self.__weekends = {}
         self.__holidays = {}
 
 
     def spot_for(self, ccy1, ccy2, trade_date):
-        result = trade_date + self.__TWO_DAYS
+        result = trade_date + self.__DEFAULT_SPOT_LAG
         weekends = self.__weekends.get(ccy1, self.__DEFAULT_WEEKENDS)
-        usd_holidays = self.__holidays.get('USD', {})
+        usd_holidays = self.__holidays.get('USD', self.__DEFAULT_HOLIDAYS)
 
         while result.isoweekday() in weekends or result in usd_holidays:
             result += self.__ONE_DAY
