4ee0bb12cc1389a217c39a3bf4dcf3067e71fe16
shaolang <shaolang@ymail.com>
2021-09-09 09:36:19 +0800

    Move calculation to class s.ValueDateCalculator

--- src/settler/__init__.py
+++ src/settler/__init__.py
@@ -1,12 +1,31 @@
 from datetime import timedelta
 
-ONE_DAY = timedelta(days=1)
-TWO_DAYS = timedelta(days=2)
 
-def spot(trade_date, weekends=[6, 7], *, usd_holidays=[]):
-    result = trade_date + TWO_DAYS
+class ValueDateCalculator:
+    __DEFAULT_WEEKENDS = [6, 7]
+    __ONE_DAY = timedelta(days=1)
+    __TWO_DAYS = timedelta(days=2)
 
-    while result.isoweekday() in weekends or result in usd_holidays:
-        result += ONE_DAY
+    def __init__(self):
+        self.__spot_lag = timedelta(days=2)
+        self.__weekends = {}
+        self.__holidays = {}
 
-    return result
+
+    def spot_for(self, ccy1, ccy2, trade_date):
+        result = trade_date + self.__TWO_DAYS
+        weekends = self.__weekends.get(ccy1, self.__DEFAULT_WEEKENDS)
+        usd_holidays = self.__holidays.get('USD', {})
+
+        while result.isoweekday() in weekends or result in usd_holidays:
+            result += self.__ONE_DAY
+
+        return result
+
+
+    def set_holidays(self, ccy, holidays):
+        self.__holidays[ccy] = holidays
+
+
+    def set_weekends(self, ccy1, weekends):
+        self.__weekends[ccy1] = weekends
--- test/spot_test.py
+++ test/spot_test.py
@@ -6,6 +6,15 @@ import settler as s
 
 # -- generators --
 
+def currency_pairs():
+    gen_ccy = st.one_of(
+            st.lists(st.characters(min_codepoint=48, max_codepoint=91),
+                min_size=3, max_size=3).map(lambda cs: ''.join(cs)),
+            st.sampled_from(['USD', 'AUD', 'JPY', 'SGD']))
+
+    return st.lists(gen_ccy, min_size=2, max_size=2, unique=True)
+
+
 def trade_dates():
     return st.dates(date(2000, 1, 1), date(2050, 12, 31))
 
@@ -13,26 +22,40 @@ def trade_dates():
 @st.composite
 def trade_dates_and_usd_holidays(draw):
     trade_date = draw(trade_dates())
-    days_to_add = draw(st.lists(st.integers(0, 20).map(lambda n: trade_date + timedelta(days=n))))
+    days_to_add = draw(
+            st.lists(st.integers(0, 20)
+                .map(lambda n: trade_date + timedelta(days=n))))
 
     return (trade_date, days_to_add)
+
 
 def weekend_lists():
     return st.lists(st.integers(1, 7), max_size=3, unique=True)
 
 # -- properties --
 
-@given(trade_dates())
-def test_spot_date_never_falls_on_weekend(trade_date):
-    assert s.spot(trade_date).isoweekday() < 6
+@given(trade_dates(), currency_pairs())
+def test_spot_date_never_falls_on_weekend(trade_date, pair):
+    calculator = s.ValueDateCalculator()
+    ccy1, ccy2 = pair
+
+    assert calculator.spot_for(ccy1, ccy2, trade_date).isoweekday() < 6
 
 
-@given(weekend_lists(), trade_dates())
-def test_weekends_arent_always_sat_and_sun_only(weekends, trade_date):
-    assert s.spot(trade_date, weekends).isoweekday() not in weekends
+@given(weekend_lists(), trade_dates(), currency_pairs())
+def test_weekends_arent_always_sat_and_sun_only(weekends, trade_date, pair):
+    ccy1, ccy2 = pair
+    calculator = s.ValueDateCalculator()
+    calculator.set_weekends(ccy1, weekends)
+
+    assert calculator.spot_for(ccy1, ccy2, trade_date).isoweekday() not in weekends
 
 
-@given(trade_dates_and_usd_holidays())
-def test_spot_never_falls_on_usd_holidays(trade_date_and_usd_holidays):
+@given(trade_dates_and_usd_holidays(), currency_pairs())
+def test_spot_never_falls_on_usd_holidays(trade_date_and_usd_holidays, pair):
     trade_date, usd_holidays = trade_date_and_usd_holidays
-    assert s.spot(trade_date, usd_holidays=usd_holidays) not in usd_holidays
+    ccy = pair[1] if pair[0] == 'USD' else pair[0]
+    calculator = s.ValueDateCalculator()
+    calculator.set_holidays('USD', usd_holidays)
+
+    assert calculator.spot_for(ccy, 'USD', trade_date) not in usd_holidays
